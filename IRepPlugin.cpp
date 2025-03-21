2c6b6e4dd21837de9e9fc9706a2550e6f8fd05e5
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-03-21 11:05:26 -0400

    Updated to newer version

--- IRepPlugin.cpp
+++ IRepPlugin.cpp
@@ -21,7 +21,27 @@ void IRepPlugin::run() {}
 void IRepPlugin::output(std::string file) {
  std::string outputfile = file;
  std::string myCommand = "";
+ if (parameters.count("sample1") != 0) {
  myCommand += "iRep -f "+parameters["target"]+" -o "+file+" -ff -s "+parameters["sample1"]+" "+parameters["sample2"];
+ myCommand += "iRep -f "+parameters["target"]+" -o "+file+" -ff -s "+parameters["sample1"]+" "+parameters["sample2"];
+ }
+ else{
+//iRep -f GCA_002903045.1_ASM290304v1_genomic.fna -s output.sam -o output-iRep
+ myCommand += "iRep -ff -f ";
+ std::ifstream infile(PluginManager::addPrefix(parameters["target"]), std::ios::in);
+ std::vector<std::string> myfiles;
+ std::string onefile;
+ while (!infile.eof()) {
+    getline(infile, onefile);
+    if (onefile.length() != 0)
+	    myfiles.push_back(onefile);
+ }
+ for (int i = 0; i < myfiles.size(); i++){
+	 myCommand += PluginManager::addPrefix(myfiles[i]);
+	 myCommand += " ";
+ }
+ myCommand += " -s "+PluginManager::addPrefix(parameters["sample"])+" -o "+file;
+ }
  std::cout << myCommand << std::endl;
  system(myCommand.c_str());
 }
